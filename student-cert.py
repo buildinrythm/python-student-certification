a59069b0b55fece16d76d7c15a91d16df1dcc5e4
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-01-08 08:59:39 +0000

    Functionn to print results from users

--- student-cert.py
+++ student-cert.py
@@ -56,3 +56,24 @@ def process_candidate():
         average,
         overall_result
     )
+
+def print_certificate(name, cert_name, modules, results, average, overall):
+    print("\n======================================")
+    print("        CERTIFICATION RESULT")
+    print("======================================")
+    print(f"Candidate Name: {name}")
+    print(f"Certification: {cert_name}\n")
+
+    print("Module Results:")
+    for i in range(len(modules)):
+        grade, result = get_grade_and_result(results[i])
+        print(
+            f"{modules[i]:20} "
+            f"Score: {results[i]:3}% "
+            f"Grade: {grade} "
+            f"Result: {result}"
+        )
+
+    print(f"\nOverall Average: {float(average):.2f}%")
+    print(f"Final Outcome: {overall}")
+    print("======================================\n")
