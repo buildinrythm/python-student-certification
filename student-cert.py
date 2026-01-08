14a2abdfd946486e1f55711bc0a98e0183c5d77e
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-01-08 08:55:34 +0000

    Wrote function to get grade

--- student-cert.py
+++ student-cert.py
@@ -6,3 +6,15 @@ def display_welcome():
     print("======================================")
     print(" Welcome to the Certification System ")
     print("======================================\n")
+
+def get_grade_and_result(score):
+    if 70 <= score <= 100:
+        return "A", "Pass"
+    elif 60 <= score <= 69:
+        return "B", "Pass"
+    elif 50 <= score <= 59:
+        return "C", "Pass"
+    elif 40 <= score <= 49:
+        return "D", "Fail"
+    else:
+        return "E", "Fail"
